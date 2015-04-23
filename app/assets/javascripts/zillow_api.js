442fc6019afbb7d93b3f4352f7602286123b12d6
Yumiko Huang <dayumikoda@gmail.com>
2015-04-23 11:19:45 -0700

    zillow api calls needed to be made from Rails

--- app/assets/javascripts/zillow_api.js
+++ app/assets/javascripts/zillow_api.js
@@ -2,22 +2,28 @@
 
 $(function(){
 	$.ajax({
-		url:'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1a9jqja8op7_1u0pu&address=1926+M+L+KING+JR+Way&citystatezip=Oakland%2C+CA',
+		url:'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1a9jqja8op7_1u0pu&address=1926+M+L+KING+JR+Way&citystatezip=Oakland%2C+CA&callback=callback',
 		type: 'get',
-		dataType: 'xml'
-	}).done(function(response){
+		dataType: 'jsonp',
+		jsonp: 'callback'
+	}).done(function(serverData){
+		var ppp = xmlToJson(serverData)
 		console.log('success');
-		console.log(response);
+		console.log(ppp);
 		debugger;
 	}).fail(function(err){
 		console.log('error');
 	})
 });
 
+// function callback_func(response){
+// 	console.log(response);
+// }
+
 // (function(){
 // 	var zillowReq = function(){
 // 		var xhr = new XMLHttpRequest();
-// 		xhr.open('GET', 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1a9jqja8op7_1u0pu&address=1926+M+L+KING+JR+Way&citystatezip=Oakland%2C+CA');
+// 		xhr.open('GET', 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1a9jqja8op7_1u0pu&address=1926+M+L+KING+JR+Way&citystatezip=Oakland%2C+CA?callback=callback_func');
 // 		xhr.onload = function() {
 // 			console.log(xhr.responseText);
 // 		  var zillowRes = xmlToJson(xhr.responseText);
@@ -28,3 +34,4 @@ $(function(){
 //   zillowReq();
   
 // })();
+
