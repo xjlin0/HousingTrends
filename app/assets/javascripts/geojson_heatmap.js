7e5fd868a6d260297c710992432c84526431763d
Yumiko Huang <dayumikoda@gmail.com>
2015-04-22 23:31:18 -0700

    search bar added on the map, marker built, information box able to be displayed above the marker.

--- app/assets/javascripts/geojson_heatmap.js
+++ app/assets/javascripts/geojson_heatmap.js
@@ -2,7 +2,8 @@
 	var map, pointarray, heatmap, toggleHeatmap, boundary;
 	var data_set_one = [],
 			data_set_two = [],
-			data_set_three = [];
+			data_set_three = [],
+			markers = [];
 	var mapSetup = function(){
 		var mapOptions = {
 		  zoom: 11,
@@ -13,8 +14,65 @@
 
 		map = new google.maps.Map(document.getElementById('heatmap-canvas'),
 		    mapOptions);
+
+		var input = (document.getElementById('pac-input'));
+		map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+		var searchBox = new google.maps.places.SearchBox((input));
+
+		google.maps.event.addListener(searchBox, 'places_changed', function() {
+		  var places = searchBox.getPlaces();
+
+		  if (places.length == 0) {
+		    return;
+		  }
+		  for (var i = 0, marker; marker = markers[i]; i++) {
+		    marker.setMap(null);
+		  }
+
+		  // For each place, get the icon, place name, and location.
+		  markers = [];
+		  boundary = new google.maps.LatLngBounds();
+		  for (var i = 0, place; place = places[i]; i++) {
+		  	// image can be an URL
+		    var image = {
+		      url: place.icon,
+		      size: new google.maps.Size(71, 71),
+		      origin: new google.maps.Point(0, 0),
+		      anchor: new google.maps.Point(17, 34),
+		      scaledSize: new google.maps.Size(25, 25)
+		    };
+
+		    var contentString = 'testing';
+
+		    var infowindow = new google.maps.InfoWindow({
+		        content: contentString
+		    });
+
+		    // Create a marker for each place.
+		    var marker = new google.maps.Marker({
+		      map: map,
+		      icon: image,
+		      // title: place.name,
+		      title: place.name,
+		      position: place.geometry.location
+		    });
+
+		    google.maps.event.addListener(marker, 'click', function() {
+		      infowindow.open(map,marker);
+		    });
+
+		    markers.push(marker);
+
+		    boundary.extend(place.geometry.location);
+		  }
+
+		  map.fitBounds(boundary);
+		});
+
 		google.maps.event.addListener(map, 'bounds_changed', function() {
         boundary = map.getBounds();
+        searchBox.setBounds(boundary);
     });
 	}
 
